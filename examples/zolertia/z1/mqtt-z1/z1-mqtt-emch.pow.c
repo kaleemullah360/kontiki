2c62bfe11e36e73b0e2ee38eb1f449ab64e3d8ab
kaleemullah360 <kaleemullah360@live.com>
2016-04-25 14:31:55 +0500

    updates

--- examples/zolertia/z1/mqtt-z1/z1-mqtt-emch.pow.c
+++ examples/zolertia/z1/mqtt-z1/z1-mqtt-emch.pow.c
@@ -53,7 +53,7 @@ char *powertrace_result();
 // Set the Radio performance
 #include <cc2420.h>
 uint8_t radioChannel = 25;  // default channel
-uint8_t radioChannel_tx_power = 0; // default power
+uint8_t radioChannel_tx_power = 31; // default power
 //--- Libs for e-MCH-APp ----
 
 #include "dev/battery-sensor.h"
