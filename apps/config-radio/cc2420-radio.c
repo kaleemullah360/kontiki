15ddab744059672e565d9dd788d656fe2459a33d
kaleemullah360 <kaleemullah360@live.com>
2016-06-06 16:28:20 +0500

    cc2420-radio lib added

--- apps/config-radio/cc2420-radio.c
+++ apps/config-radio/cc2420-radio.c
@@ -31,10 +31,17 @@
 
 /**
  * \file
- *         cc2420 Radio configurations
+ *  cc2420 Radio configurations
  * \author
- *         Kaleem Ullah <mscs14059@itu.edu.pk>
- *         Kaleem Ullah <kaleemullah360@live.com>
+ *
+ *	Kaleem Ullah <mscs14059@itu.edu.pk>
+ *	Kaleem Ullah <kaleemullah360@live.com>
+ *
+ * \Usage
+ *
+ *	Case 1:	zero is passed in power/channel function i.e set_cc2420_txpower(0); will use predefined channel number in cc2420-radio lib.
+ *	Case 2:	a custom value is passed in power/channel function i.e set_cc2420_txpower(13); will use channel 13.
+ *	Case 3:	not using both/eithere of power/channel setting function will set Contiki OS default configurations i.e channel 26, tx power 31.
  */
 
 /* -------- Set Radio Powers --------------- */
