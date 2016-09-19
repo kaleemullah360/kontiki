b76a86a849a8284c23d186bb51d4ff4cc5797ef4
kaleemullah360 <kaleemullah360@live.com>
2016-09-19 10:11:25 +0500

    updates

---
+++ life.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+################################################################################
+# 1. This script is used to check whether your cpu is functional or not.       #
+# Written By:                                                                  #
+#  Kaleem Ullah <mscs14059@itu.edu.pk> <kaleemullah360@live.com>               #
+################################################################################
+
+yes !===================================[ msg: You are jacked! now go home. by Kaleem Ullah -Su ]================================================!
+
