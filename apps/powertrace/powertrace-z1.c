7c18c32af046b82b23aa9de396871de6a8976c27
kaleemullah360 <kaleemullah360@live.com>
2016-09-28 13:36:21 +0500

    updates

--- apps/powertrace/powertrace-z1.c
+++ apps/powertrace/powertrace-z1.c
@@ -131,6 +131,7 @@ source: http://personal.ee.surrey.ac.uk/Personal/P.Barnaghi/teaching/EEEM048/201
   // instead of printing, write all results to powertrace_results_str and return to calling function
   // => simple version
   sprintf(powertrace_results_str, "%lu,%lu,%lu,%lu,%lu,%lu\n",all_cpu, all_lpm, all_transmit, all_listen, all_idle_transmit, all_idle_listen);
+
 /*
   // => full version
   sprintf(powertrace_results_str, "%s %lu P %d.%d %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu (%d.%02d%% / %d.%02d%% tx %d.%02d%% / %d.%02d%% rx %d.%02d%% / %d.%02d%%)\n",
