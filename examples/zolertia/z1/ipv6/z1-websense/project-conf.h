47c485dd89bcb6f9dd46c072cbcb2ec9cb104dd7
kaleemullah360 <kaleemullah360@live.com>
2016-10-20 20:06:44 +0500

    updates

--- examples/zolertia/z1/ipv6/z1-websense/project-conf.h
+++ examples/zolertia/z1/ipv6/z1-websense/project-conf.h
@@ -27,6 +27,11 @@
  * SUCH DAMAGE.
  *
  */
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC nullrdc_driver
+
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC nullmac_driver
 
 #ifndef PROJECT_RPL_WEB_CONF_H_
 #define PROJECT_RPL_WEB_CONF_H_
